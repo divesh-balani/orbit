d519e05bcc88cf1a83a54bf1cf17662db7d1723f
Divesh Balani <diveshb2k@gmail.com>
2026-03-23 01:32:05 +0100

    fix: correct VIDCAP constant names in camera-mediafoundation

--- crates/camera-mediafoundation/src/lib.rs
+++ crates/camera-mediafoundation/src/lib.rs
@@ -45,7 +45,7 @@ impl DeviceSourcesIterator {
         unsafe {
             attributes.SetGUID(
                 &MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE,
-                &MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE_VIDORBIT_GUID,
+                &MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE_VIDCAP_GUID,
             )?;
         }
 
@@ -148,7 +148,7 @@ impl Device {
         unsafe {
             self.activate
                 .GetAllocatedString(
-                    &MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE_VIDORBIT_SYMBOLIC_LINK,
+                    &MF_DEVSOURCE_ATTRIBUTE_SOURCE_TYPE_VIDCAP_SYMBOLIC_LINK,
                     &mut raw,
                     &mut length,
                 )
@@ -455,9 +455,6 @@ impl SourceReader {
             == MF_SOURCE_READERF_CURRENTMEDIATYPECHANGED.0
         {
             return Ok(None);
-            // selected_format =
-            //     VideoFormat::new(reader.GetCurrentMediaType(stream_index).unwrap())
-            //         .unwrap();
         }
 
         Ok(Some(VideoSample(imf_sample)))
